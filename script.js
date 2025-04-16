17d9081d0f0406ec3a61a1c46edcfeb1e93b540b
Marcus Melo <marcus.melo@dcomp.ufs.br>
2025-04-16 13:38:20 -0300

    att

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const testimonialCarousel = new bootstrap.Carousel('#testimonialCarousel', {
+    interval: 5000,
+    ride: 'carousel'
+  });
+});
