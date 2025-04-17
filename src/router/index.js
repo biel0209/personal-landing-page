924e6cc2d633bfbe917abc6e4c1485ce62a61bea
Marcus Melo <marcus.melo@dcomp.ufs.br>
2025-04-17 16:17:39 -0300

    att

--- src/router/index.js
+++ src/router/index.js
@@ -8,7 +8,7 @@ const routes = [
 ]
 
 export default createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes,
   scrollBehavior(to, from, savedPosition) {
     if (to.hash) {
