c4d6c6891a00107d33f65082546194eee02c451c
Marcus Melo <marcus.melo@dcomp.ufs.br>
2025-04-17 15:31:31 -0300

    deploy

--- src/main.js
+++ src/main.js
@@ -3,7 +3,4 @@ import App from './App.vue';
 import router from './router';
 import './assets/styles.css';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-
 createApp(App).use(router).mount('#app');
