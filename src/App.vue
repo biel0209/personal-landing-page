baa594a7ac3ee4822f7fe021b9690dce2937a048
Marcus Melo <marcus.melo@dcomp.ufs.br>
2025-05-21 10:49:15 -0300

    add vercel analytics

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <script setup>
 import Menu from './components/Menu.vue';
 import Footer from './components/Footer.vue';
+import { Analytics } from '@vercel/analytics/vue';
 </script>
 
 <template>
+  <Analytics />
   <Menu />
   <router-view />
   <Footer />
 </template>
-
