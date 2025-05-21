12d039d35d496889b3d953245950227c14845a1b
Marcus Melo <marcus.melo@dcomp.ufs.br>
2025-05-21 10:53:21 -0300

    add speed insights

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,12 @@
 import Menu from './components/Menu.vue';
 import Footer from './components/Footer.vue';
 import { Analytics } from '@vercel/analytics/vue';
+import { SpeedInsights } from '@vercel/speed-insights/vue';
 </script>
 
 <template>
   <Analytics />
+  <SpeedInsights />
   <Menu />
   <router-view />
   <Footer />
